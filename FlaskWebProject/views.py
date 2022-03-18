2c13b0bc5ce988095844eea2712a616489cb170e
Bach Nguyen Viet <ngvbach2000@gmail.com>
2022-03-18 12:21:50 +0700

    Update logging

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -63,8 +63,10 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+            app.logger.warning('Invalid login attempt')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        app.logger.warning(form.username.data + ' logged in successfully')
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
